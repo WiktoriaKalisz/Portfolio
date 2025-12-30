6dada8352ec0b12d8884a5e33adec922f22b2f00
Wiktoria Kalisz <wiktoriakalisz@Marcins-MacBook-Pro.local>
2025-12-30 14:04:02 +0100

    Minor code formatting improvements

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
 import { getImageUrl } from '../../utils'
 import styles from "./Hero.module.css"
 
 export const Hero = () => {
-  return (
-    <>
-  <section className={styles.container}>
-    <div className={styles.content}>
-    <h1 className={styles.title}>Hi, I'm Wiktoria</h1>
-    <p className={styles.description}>
-    Curious-minded developer with growing experience in front-end development — and a soft spot for thoughtful, beautifully crafted design. Quick to catch bugs and drawn to technologies like React, Next.js, and TypeScript. Eager to keep learning, collaborate closely, and help build clean, accessible UIs that feel just right.
-    </p>
-    </div>
-    <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
-  
-  </section>
-  <div className={styles.waveTopWrapper}>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#E7E7E7" fill-opacity="1" d="M0,128L48,160C96,192,192,256,288,250.7C384,245,480,171,576,154.7C672,139,768,181,864,197.3C960,213,1056,203,1152,208C1248,213,1344,235,1392,245.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-  </div>
-  </>
-  );
-};
+	return (
+		<>
+			<section className={styles.container}>
+				<div className={styles.content}>
+					<h1 className={styles.title}>Hi, I'm Wiktoria</h1>
+					<p className={styles.description}>
+						Curious-minded developer with growing experience in front-end development — and a soft spot for thoughtful, beautifully crafted design. Quick to catch bugs and drawn to technologies like React, Next.js, and TypeScript. Eager to keep learning, collaborate closely, and help build clean, accessible UIs that feel just right.
+					</p>
+				</div>
+				<img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
+			</section>
+			<div className={styles.waveTopWrapper}>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#E7E7E7" fill-opacity="1" d="M0,128L48,160C96,192,192,256,288,250.7C384,245,480,171,576,154.7C672,139,768,181,864,197.3C960,213,1056,203,1152,208C1248,213,1344,235,1392,245.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+			</div>
+		</>
+	)
+}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,17 +4,26 @@ import { ProjectCard } from './ProjectCard';
 import styles from "./Projects.module.css"
 
 export const Projects = () => {
-  return (<>
-  <div className={styles.waveTopWrapper}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{ backgroundColor: "#DEB472" }}><path fill="#E7E7E7" d="M0,192L48,202.7C96,213,192,235,288,234.7C384,235,480,213,576,181.3C672,149,768,107,864,90.7C960,75,1056,85,1152,85.3C1248,85,1344,75,1392,69.3L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg></div>
-  <section className={styles.container} id="projects">
-    <h2 className={styles.title}>Projects</h2>
-  <div className={styles.projects}>{projects.map((project,id) => {
-    return <ProjectCard key={id} project={project}/>;
-    })}
-    </div>
-  </section>
-  <div className={styles.waveTopWrapper}>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{ backgroundColor: "#383537" }}  ><path fill="#DEB472" fill-opacity="1" d="M0,224L48,186.7C96,149,192,75,288,74.7C384,75,480,149,576,170.7C672,192,768,160,864,133.3C960,107,1056,85,1152,96C1248,107,1344,149,1392,170.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-  </div>
-  </>);
+  return (
+    <>
+      <div className={styles.waveTopWrapper}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{ backgroundColor: "#DEB472" }}>
+          <path fill="#E7E7E7" d="M0,192L48,202.7C96,213,192,235,288,234.7C384,235,480,213,576,181.3C672,149,768,107,864,90.7C960,75,1056,85,1152,85.3C1248,85,1344,75,1392,69.3L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+        </svg>
+      </div>
+      <section className={styles.container} id="projects">
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projects}>
+          {projects.map((project, id) => {
+            return <ProjectCard key={id} project={project} />;
+          })}
+        </div>
+      </section>
+      <div className={styles.waveTopWrapper}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style={{ backgroundColor: "#383537" }}>
+          <path fill="#DEB472" fill-opacity="1" d="M0,224L48,186.7C96,149,192,75,288,74.7C384,75,480,149,576,170.7C672,192,768,160,864,133.3C960,107,1056,85,1152,96C1248,107,1344,149,1392,170.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+        </svg>
+      </div>
+    </>
+  )
 }
